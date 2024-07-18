862cf7a372e5b3698a0e24fae3ddbf4a7b63f02e
aslihan-gurkan <aslihangurkan07@gmail.com>
2024-07-18 21:41:22 +0300

    Update app.js

--- app.js
+++ app.js
@@ -6,3 +6,29 @@ const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
+app.use(cors());
+
+const eCodes = [
+    {
+        "code": "100",
+        "name": "Curcuma",
+        "Description": "Harmful bla bla",
+        "Category": "Harmful"
+    },
+    {
+        "code": "101",
+        "name": "Riboflavin",
+        "Description": "Vitamin B2",
+        "Category": "Harmless"
+    },
+
+];
+
+app.get('/api/ecodes', (req, res) =>
+{
+    res.json(eCodes);
+});
+
+app.listen(port, () => {
+    console.log('E-Code API listening at http:/Localhost:${port}');
+});
